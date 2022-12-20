cfcb0caec22f9ddd6a68708588a4a56f47a23f44
user <katalonij440@gmail.com>
2022-12-20 18:09:55 +0200

    add footer and modal window (library hystmodal)

---
+++ src/js/modalOurTeam.js
@@ -0,0 +1,13 @@
+import HystModal from 'hystmodal';
+import 'hystmodal/dist/hystmodal.min.css';
+import { refs } from './refs';
+
+refs.openOurTeam.addEventListener('click', onClick);
+
+const myModal = new HystModal({
+  linkAttributeName: 'data-hystmodal',
+});
+
+function onClick() {
+  myModal.init();
+}

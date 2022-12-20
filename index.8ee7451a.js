5b5d2c45c18e20c1e1e1aa66c46c51b5b6eb977b
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-12-20 16:38:19 +0000

    Deploying to gh-pages from @ iankaFil/project-group-5-js@5aff121ed48c615564229d856ad4e75af709013d 🚀

--- index.8ee7451a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".gallery"),document.querySelector(".genres__list"),document.querySelector(".gallery__card"),document.querySelector(".header"),document.querySelector("#home"),document.querySelector("#library"),document.querySelector(".header__wrap--form"),document.querySelector(".header__wrap--btn"),document.querySelector(".header__wrap");
-//# sourceMappingURL=index.8ee7451a.js.map

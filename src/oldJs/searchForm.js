14fb76b8f0153b5db54f9ace11a6e728203c3896
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-12-22 00:57:05 -0700

    upd search form

--- src/oldJs/searchForm.js
+++ src/oldJs/searchForm.js
@@ -1,30 +1,30 @@
-import { refs } from './refs';
+// import { refs } from './refs';
 
-console.log(refs.searchForm);
-console.log(refs.searchMovieInput);
+// console.log(refs.searchForm);
+// console.log(refs.searchMovieInput);
 
-refs.searchForm.addEventListener('submit', checkForm); // проверка формы при поиске фильма
+// refs.searchForm.addEventListener('submit', checkForm); // проверка формы при поиске фильма
 
-//  проверка данных  в форме и если все гуд то отправка
-function checkForm(event) {
-  event.preventDefault();
+// //  проверка данных  в форме и если все гуд то отправка
+// function checkForm(event) {
+//   event.preventDefault();
 
-  let inputValue = refs.searchForm.elements.search.value.trim();
-  // inputValue = inputValue.trim();
+//   let inputValue = refs.searchForm.elements.search.value.trim();
+//   // inputValue = inputValue.trim();
 
-  if (inputValue.length === 0) {
-    console.log('000');
-    return false;
-  } else {
-    refs.searchForm.elements.search.value = inputValue;
-    refs.searchForm.submit();
-  }
-}
+//   if (inputValue.length === 0) {
+//     console.log('000');
+//     return false;
+//   } else {
+//     refs.searchForm.elements.search.value = inputValue;
+//     refs.searchForm.submit();
+//   }
+// }
 
-function searchWordToInput() {
-  const currentURL = window.location.href;
-  const searchWord = new URL(currentURL).searchParams.get('search');
-  if (searchWord !== null) {
-    searchMovieInput.value = searchWord.trim();
-  }
-}
+// function searchWordToInput() {
+//   const currentURL = window.location.href;
+//   const searchWord = new URL(currentURL).searchParams.get('search');
+//   if (searchWord !== null) {
+//     searchMovieInput.value = searchWord.trim();
+//   }
+// }

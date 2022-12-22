e072eaca72ddd744cb9c1ed4f30eaf0f7375e1ce
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-12-22 16:22:50 -0700

    settings

--- src/oldJs/loader.js
+++
@@ -1,7 +0,0 @@
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-
-const startLoading = () => Loading.hourglass('Loading...');
-
-const stopLoading = () => Loading.remove();
-
-export { startLoading, stopLoading };

97c96a32c875bc005637de2f2df3e75d1530f41d
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-12-23 03:06:04 -0700

    upd backdrop

--- src/js/backdrop.js
+++ src/js/backdrop.js
@@ -75,13 +75,13 @@ function renderMovieDetails(data) {
 
 refs.iconClose.addEventListener('click', () => {
   refs.backdrop.classList.add('is-hidden');
-  // refs.bodyEl.classList.remove('hidden');
+  refs.bodyEl.classList.remove('hidden');
 })
 refs.backdrop.addEventListener('click', ({ target }) => {
   // закрытие бэкдропа
   if (target === refs.backdrop) {
     refs.backdrop.classList.add('is-hidden');
-    // refs.bodyEl.classList.remove('hidden');
+    refs.bodyEl.classList.remove('hidden');
   }
 
   if (target.tagName === 'BUTTON' && target.classList.contains('js-watched')) {

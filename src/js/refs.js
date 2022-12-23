21b8c27ade431093a655b35d4c92709609f16d67
Alexander Kushnir <dj.egregor1@gmail.com>
2022-12-23 11:36:10 +0100

    add button and container

--- src/js/refs.js
+++ src/js/refs.js
@@ -10,5 +10,5 @@ export const refs = {
   notificationWarning: document.querySelector('.header__warning'),
   themeToggle: document.querySelector('.themetoggle'),
   bodyEl: document.body,
-  iconClose: document.querySelector('.icon__close'),
+  buttonCloseBackdrop: document.querySelector('.js-close-backdrop'),
 };

d43569c100da0ffb34421cd7af5438a5c2cca6f9
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-12-24 18:38:55 -0700

    Update backdrop.js

--- src/js/backdrop.js
+++ src/js/backdrop.js
@@ -18,11 +18,10 @@ function renderMovieDetails(data) {
 
   const content = `
   <div class="image-wrap">
-  <img class="movie-detail__image" ${
-    data.poster_path
+  <img class="movie-detail__image" ${data.poster_path
       ? 'src="https://image.tmdb.org/t/p/w300' + data.poster_path + '">'
       : 'src="' + noImg + '">'
-  }
+    }
   </div>
 
     <div class="info-wrap">
@@ -32,10 +31,9 @@ function renderMovieDetails(data) {
           <tr>
             <td><span class="movie-detail__title-table-titles">Vote / Votes</span></td>
             <td><span class="movie-detail__rating">${data.vote_average.toFixed(
-              1
-            )}</span>/<span class="movie-detail__info movie-votes">${
-    data.vote_count
-  }</span></td>
+      1
+    )}</span>/<span class="movie-detail__info movie-votes">${data.vote_count
+    }</span></td>
           </tr>
           <tr>
             <td><span class="movie-detail__title-table-titles">Popularity</span></td>
@@ -43,15 +41,14 @@ function renderMovieDetails(data) {
           </tr>
           <tr>
             <td><span class="movie-detail__title-table-titles">Original Title</span></td>
-            <td><span class="movie-detail__info movie-detail__info--uppercase">${
-              data.original_title
-            }</span></td>
+            <td><span class="movie-detail__info movie-detail__info--uppercase">${data.original_title
+    }</span></td>
           </tr>
           <tr>
             <td><span class="movie-detail__title-table-titles">Genre</span></td>
             <td><span class="movie-detail__info">${getGenre(
-              data.genres
-            )}</span></td>
+      data.genres
+    )}</span></td>
           </tr>
         </tbody>
       </table>
@@ -61,13 +58,11 @@ function renderMovieDetails(data) {
       </p>
       <div class="movie-detail__buttons-wrapper">
         <button
-          class="movie-detail__button js-watched" data-id="${
-            data.id
-          }" type="button">add to Watched</button>
+          class="movie-detail__button js-watched" data-id="${data.id
+    }" type="button">add to Watched</button>
         <button
-          class="movie-detail__button js-queue" data-id="${
-            data.id
-          }" type="button">add to queue</button>
+          class="movie-detail__button js-queue" data-id="${data.id
+    }" type="button">add to queue</button>
       </div>
     </div>
   `;
@@ -136,11 +131,9 @@ function addAndRemoveToWatched() {
       String(buttonJsWatched.dataset.id)
     )
   ) {
-    buttonJsWatched.textContent = 'remove from watched';
-    buttonJsWatched.classList.add('highlighted');
+    onToggleClassAndText('add', buttonJsWatched, 'remove from watched')
   } else {
-    buttonJsWatched.classList.remove('highlighted');
-    buttonJsWatched.textContent = 'add to watched';
+    onToggleClassAndText('remove', buttonJsWatched, 'add to watched')
   }
 }
 function addAndRemoveToQueue() {
@@ -148,12 +141,14 @@ function addAndRemoveToQueue() {
   if (
     loadArayFromLocalStorage('queue').includes(String(buttonJsQueue.dataset.id))
   ) {
-    buttonJsQueue.textContent = 'remove from queue';
-    buttonJsQueue.classList.add('highlighted');
+    onToggleClassAndText('add', buttonJsQueue, 'remove from queue')
   } else {
-    buttonJsQueue.classList.remove('highlighted');
-    buttonJsQueue.textContent = 'add to queue';
+    onToggleClassAndText('remove', buttonJsQueue, 'add to queue')
   }
+}
+function onToggleClassAndText(method, btnName, textInBtn) {
+  btnName.classList[method]('highlighted');
+  btnName.textContent = `${textInBtn}`;
 }
 
 function closeModalbyEscape(event) {
